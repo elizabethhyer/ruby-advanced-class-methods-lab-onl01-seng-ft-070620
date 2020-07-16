f2ff9191a6e462623639e5f41a22ec3f582d9a73
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-16 21:07:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,8 @@ class Song
     self.all.sort_by { |song| song.name }
   end 
   
-  def self.new_from_filename
+  def self.new_from_filename(mp3)
+    
   end 
   
   def self.create_from_filename
