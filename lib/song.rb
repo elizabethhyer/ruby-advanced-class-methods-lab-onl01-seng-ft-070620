5c100512212293214aa93f56b9973110a34b0d07
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-16 21:24:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,8 +39,8 @@ class Song
   def self.new_from_filename(mp3)
    name_and_artist = mp3.split(" - ")
    binding.pry
-   artist_name = artist_name[0]
-   name = name[1]
+   #artist_name = artist_name[0]
+   #name = name[1]
    
   end 
   
